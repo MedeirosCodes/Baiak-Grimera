32cfb38be0c48d10700f1b980bf63eae52b791da
Mateus Medeiros <mateusowmedeiross@gmail.com>
2022-09-03 12:17:31 -0300

    bosses

--- data/actions/scripts/quests/baiakAnihi.lua
+++ data/actions/scripts/quests/baiakAnihi.lua
@@ -1,33 +1,44 @@
-
-if item.uid == 5001 then
-queststatus = getPlayerStorageValue(cid,100)
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+if item.uid == 6000 then
+queststatus = getPlayerStorageValue(cid,6000)
 if queststatus == -1 then
-doPlayerSendTextMessage(cid,22,"You have found a ring of honour.")
+doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR, "Você escolheu Ring of Honour.")
 doPlayerAddItem(cid,5785,1)
-setPlayerStorageValue(cid,100,1)
+setPlayerStorageValue(cid,6000,1)
 else
-doPlayerSendTextMessage(cid,22,"It is empty.")
+doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR, "Você ja escolheu seu premio.")
 end
-end
-if item.uid == 5002 then
-queststatus = getPlayerStorageValue(cid,100)
-if queststatus ~= 1 then
-doPlayerSendTextMessage(cid,22,"You have found a blessed armor.")
+elseif item.uid == 6001 then
+queststatus = getPlayerStorageValue(cid,6000)
+if queststatus == -1 then
+doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR, "Você escolheu Blessed Armor")
 doPlayerAddItem(cid,2660,1)
-setPlayerStorageValue(cid,100,1)
+setPlayerStorageValue(cid,6000,1)
 else
-doPlayerSendTextMessage(cid,22,"It is empty.")
+doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR, "Você ja escolheu seu premio.")
 end
-end
-if item.uid == 5003 then
-queststatus = getPlayerStorageValue(cid,100)
-if queststatus ~= 1 then
-doPlayerSendTextMessage(cid,22,"You have found a thunder paladin legs.")
+elseif item.uid == 6002 then
+queststatus = getPlayerStorageValue(cid, 6000)
+if queststatus == -1 then
+doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR, "Você escolheu Thunder Paladin Legs.")
 doPlayerAddItem(cid,9777,1)
-setPlayerStorageValue(cid,100,1)
+setPlayerStorageValue(cid,6000,1)
 else
-doPlayerSendTextMessage(cid,22,"It is empty.")
+doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR, "Você ja escolheu seu premio.")
 end
+elseif item.uid == 6003 then
+queststatus = getPlayerStorageValue(cid,6000)
+if queststatus == -1 then
+doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR, "Você escolheu o Presente.")
+doPlayerAddItem(cid,2160,1)
+setPlayerStorageValue(cid,6000,1)
+else
+doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR, "Você ja escolheu seu premio.")
+
 end
+else
+return 0
+end
+
 return 1
 end
