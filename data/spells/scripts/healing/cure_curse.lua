03b87b4ba0f5be60be1fc4802d810d4a785848ea
Mateus Medeiros <mateusowmedeiross@gmail.com>
2022-09-04 12:14:05 -0300

    spells

--- data/spells/scripts/healing/cure_curse.lua
+++ data/spells/scripts/healing/cure_curse.lua
@@ -5,8 +5,8 @@ combat:setParameter(COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
 combat:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 
 function onGetFormulaValues(player, level, magicLevel)
-	local min = (level / 5) + (magicLevel * 6.9) + 40
-	local max = (level / 5) + (magicLevel * 13.2) + 82
+	local min = (level / 5) + (magicLevel * 20) + 40
+	local max = (level / 5) + (magicLevel * 32) + 82
 	return min, max
 end
 
