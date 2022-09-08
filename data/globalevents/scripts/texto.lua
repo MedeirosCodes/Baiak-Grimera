07a323549d53dab85c7a1ac4c87a4f66e39ddf73
Mateus Medeiros <mateusowmedeiross@gmail.com>
2022-09-08 18:02:49 -0300

    quest bless

--- data/globalevents/scripts/texto.lua
+++ data/globalevents/scripts/texto.lua
@@ -4,7 +4,8 @@ local config = {
         ["Barco"] = { x = 154, y = 48, z = 7 },
 	["Hunts Low Level"] = { x = 152, y = 48, z = 7 },
 	["Bosses"] = { x = 150, y = 48, z = 7 },
-	["Quest Fodonas"] = { x = 145, y = 48, z = 7 },
+	["Quests"] = { x = 145, y = 48, z = 7 },
+	["Fodonas"] = { x = 145, y = 47, z = 7 },
 	["ESCOLHA O CERTO"] = { x = 203, y = 300, z = 10 }
 
     }
