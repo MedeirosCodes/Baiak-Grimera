a8f4b81e1df02764431d3a8ea774ab8e4096d7de
Mateus Medeiros <mateusowmedeiross@gmail.com>
2022-09-14 14:22:11 -0300

    monster fast

--- data/actions/scripts/quests/blessedlegs.lua
+++ data/actions/scripts/quests/blessedlegs.lua
@@ -3,7 +3,7 @@ if item.uid == 7009 then
 queststatus = getPlayerStorageValue(cid,20996)
 if queststatus == -1 then
 doPlayerSendTextMessage(cid,22,"Você Ganhou uma Blessed Legs!.")
-doPlayerAddItem(cid,8923,1 )
+doPlayerAddItem(cid,2507,1 )
 setPlayerStorageValue(cid,20996,1)
 else
 doPlayerSendTextMessage(cid,22,"Você ja fez essa quest!")
