48a61935b60ee79770e824387b6866b0e2b9c6d9
Mateus Medeiros <mateusowmedeiross@gmail.com>
2022-09-21 16:11:28 -0300

    death

--- data/creaturescripts/scripts/others/playerdeath.lua
+++ data/creaturescripts/scripts/others/playerdeath.lua
@@ -3,10 +3,10 @@ local maxDeathRecords = 5
 local maxBless = 5
 
 function onDeath(player, corpse, killer, mostDamageKiller, unjustified, mostDamageUnjustified)
-	local experience = (PLAYERLOSS_EXPERIENCE / 10)
+	local level = getLevel()
 	local loss = getConfigValue('deathLostPercent')
 		if(loss ~= nil) then
-		doPlayerSetLossPercent(cid, experience, loss * 20)
+		doPlayerSetLossPercent(cid, level, loss * 10)
 	end
 	local playerId = player:getId()
 	if nextUseStaminaTime[playerId] then
