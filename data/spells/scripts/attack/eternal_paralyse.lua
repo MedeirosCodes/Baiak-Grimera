a77aa3caff86eecd56dfc190272f0925acabca0f
Mateus Medeiros <mateusowmedeiross@gmail.com>
2022-09-23 15:25:28 -0300

    new magic paralyse

--- data/spells/scripts/attack/eternal_paralyse.lua
+++ data/spells/scripts/attack/eternal_paralyse.lua
@@ -1,7 +1,22 @@
-local combat = Combat()
-combat:setParameter(combat, COMBAT_PARAM_TARGETCASTERORTOPMOST, true)
-combat:setParameter(combat, COMBAT_PARAM_EFFECT, CONST_ME_FIREWORK_BLUE)
-combat:setArea(createCombatArea(AREA_CIRCLE5X5))
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_TARGETCASTERORTOPMOST, true)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
+
+local condition = createConditionObject(CONDITION_PARALYZE)
+setConditionParam(condition, CONDITION_PARAM_TICKS, 7000)
+setConditionFormula(condition, -0.8, 0, -0.8, 0)
+setCombatCondition(combat, condition)
+
+function onCastSpell(cid, var)
+setPlayerStorageValue(cid, 12019, 1)
+addEvent(setPlayerStorageValue, 500, cid, 12019, -1)
+if(not doCombat(cid, combat, var)) then
+return false
+end
+
+doSendMagicEffect(getThingPosition(cid), CONST_ME_MAGIC_GREEN)
+return true
+end
 
 function onGetFormulaValues(player, level, magicLevel)
 	local min = (level / 4) + (magicLevel * 5.5) + 25
