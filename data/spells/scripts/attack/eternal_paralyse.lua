df51b7cc268816b2790761cf2ddf7079321b3b82
Mateus Medeiros <mateusowmedeiross@gmail.com>
2022-09-23 14:54:38 -0300

    eternal paralyse

---
+++ data/spells/scripts/attack/eternal_paralyse.lua
@@ -0,0 +1,16 @@
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_PARAM_TARGETCASTERORTOPMOST)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_FIREWORK_BLUE)
+combat:setArea(createCombatArea(AREA_CIRCLE7X7))
+
+function onGetFormulaValues(player, level, magicLevel)
+	local min = (level / 4) + (magicLevel * 5.5) + 25
+	local max = (level / 4) + (magicLevel * 11) + 50
+	return -min, -max
+end
+
+combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
+function onCastSpell(creature, variant)
+	return combat:execute(creature, variant)
+end
